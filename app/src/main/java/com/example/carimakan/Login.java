9667d27a97a491ae770f33ba6915a650ad416469
FaadLi <37612569+FaadLi@users.noreply.github.com>
2019-12-17 20:54:46 +0700

    xml

--- app/src/main/java/com/example/carimakan/Login.java
+++ app/src/main/java/com/example/carimakan/Login.java
@@ -7,23 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
-    Button btnLogin;
+    Button btnLogin2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        btnLogin = findViewById(R.id.btn_login);
+        btnLogin2 = findViewById(R.id.btn_login2);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnLogin2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Home.class);
+                Intent i = new Intent(Login.this, Home.class);
                 startActivity(i);
             }
         });
+
 
     }
 }
